504877a1979119ae3a19d8d12f8bed4240b82ba6
Daniel Klaus <d@nielklaus.de>
2019-12-20 23:04:47 +0100

    fix calculation

--- HeavenlyCalculator.js
+++ HeavenlyCalculator.js
@@ -9,8 +9,7 @@ readline.question(`Wie viele Heavenly Chips willst du erreichen? `, (number) => 
 });
 
 function getCookies(hc) {
-    let cookies = hc ** 3 * 10 ** 12;
-    cookies = cookies / (10 ** 9);
+    let cookies = hc ** 3;      //cookies in trillions
     let modifier = 0;
     const modifiers = ['Tr', 'Quadr', 'Quint', 'Sext', 'Sept'];
     while (cookies > 1000) {
